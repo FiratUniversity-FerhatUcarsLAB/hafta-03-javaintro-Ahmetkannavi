cd737a746e69f5839f771c9798b4a74776c85a45
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-20 22:17:44 +0300

    HesapOzeti

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,13 @@
+ public class HesapOzeti {
+ public static void main(String[] args) {
+    System.out.println("Ürün  \tMiktar\tFiyat");
+    System.out.println("Süt   \t  4   \t 31 TL");
+    System.out.println("Ekmek  \t  3   \t 10 TL");
+    System.out.println("Cibs  \t  2   \t 35 TL");
+    System.out.println("yöğurt\t  1   \t 125 TL");
+
+System.out.println("------------------------");
+  int toplam = (4 * 31) + (3 * 10) + (2 * 35) + (1 * 125);
+    System.out.println("Toplam Fiyat:" + toplam +  " TL");
+ }
+  }
