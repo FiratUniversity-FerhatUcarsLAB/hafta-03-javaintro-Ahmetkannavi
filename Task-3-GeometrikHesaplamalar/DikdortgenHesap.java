df9da565e5a339727d291a654a3df82c587ba1b4
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-20 17:39:16 +0300

    Dikdortgen.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,11 @@
+public class Dikdortgen {
+    public static void main(String[] args) {
+      double en = 4.5;
+      double boy = 9.7;
+      double alan = en * boy;
+      double cevre = 2 * (en + boy);
+
+      System.out.println("Alan: " + alan);
+      System.out.println("Cevre: " + cevre);
+    }
+}
