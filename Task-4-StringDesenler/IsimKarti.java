b96a5b2fa25f78efd2ef1a8562ef5e98e9527782
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-20 21:04:25 +0300

    IsimKartitest.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+public class IsimKartitest {
+ public static void main(String[] args) {
+    String ad = "Ahmed";
+    String soyad = "Kannavi";
+    String ogrNo = "240541603";
+    String bolum = "Yazilim Muh.";
+    System.out.println("=====================");
+    System.out.println("  OGRENCI BILGILERI  ");
+    System.out.println("=====================");
+    System.out.println("Ad     : " + ad);
+    System.out.println("Soyad  : " + soyad);
+    System.out.println("OgrNo  : " + ogrNo);
+    System.out.println("Bolum  : " + bolum);
+    System.out.println("=====================");
+    }
+  }
