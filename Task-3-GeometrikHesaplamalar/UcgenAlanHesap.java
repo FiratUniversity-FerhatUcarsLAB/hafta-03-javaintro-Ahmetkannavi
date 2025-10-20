46f5b7fc1ada6157e14479f4c9e5cb73d35cfc5a
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-20 20:15:58 +0300

    UcgenAlanHesap.java

---
+++ Task-3-GeometrikHesaplamalar/UcgenAlanHesap.java
@@ -0,0 +1,9 @@
+class Main  {
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double c = 5;
+        double s = ( a + b + c ) / 2;
+        System.out.println("ucgenin alani:" + Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+    }
+}
