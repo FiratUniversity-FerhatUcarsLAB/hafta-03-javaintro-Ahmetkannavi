bca0c1d81159f0fe4c6c8686cbf2ce6a4ec8d8d0
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-23 15:43:34 +0300

    CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,15 @@
+public class UsluSayilarTablosu {
+    public static void main(String[] args)  {
+        int x =2;
+        System.out.println("2 * 1 = " + (x * 1));
+        System.out.println("2 * 2 = " + (x * 2));
+        System.out.println("2 * 3 = " + (x * 3));
+        System.out.println("2 * 4 = " + (x * 4));
+        System.out.println("2 * 5 = " + (x * 5));
+        System.out.println("2 * 6 = " + (x * 6));
+        System.out.println("2 * 7 = " + (x * 7));
+        System.out.println("2 * 8 = " + (x * 8));
+        System.out.println("2 * 9 = " + (x * 9));
+        System.out.println("2 * 10 = " + (x * 10));
+    }
+}
