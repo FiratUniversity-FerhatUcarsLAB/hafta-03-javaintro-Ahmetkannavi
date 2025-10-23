dd9bb7113ba7e9faff01eae7a5ba59e8cd883d54
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-23 15:22:17 +0300

    JavaDeseni.java

--- Task-1-HosgeldinMesajlari/JavaDeseni.java
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -1,6 +1,6 @@
 
 
-class Main {
+class JavaDeseni {
     public static void main(String[] args) {
         System.out.println("   J A V V A");
         System.out.println("  J A A V V A A ");
@@ -9,3 +9,4 @@ class Main {
         System.out.println("    J A V V A");
         }
 }
+
