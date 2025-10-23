943c0498acc1e38e6844ab47a632e12ad0d27fc7
Ahmed Kannavi <ahmetkannavi26@gmail.com>
2025-10-23 19:49:33 +0300

    MilKilometreTablosu.java
    Added a program to convert miles to kilometers and display the results.

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,15 @@
+public class MilToKm {
+    public static void main(String[] args) {
+        double km1 = 1 * 1.609;
+        double km5 = 5 * 1.609;
+        double km10 = 10 * 1.609;
+        double km50 = 50 * 1.609;
+
+        System.out.println("Mil\tKilometre");
+        System.out.println("1\t" + km1);
+        System.out.println("5\t" + km5);
+        System.out.println("10\t" + km10);
+        System.out.println("20\t" + km20);
+        System.out.println("50\t" + km50);
+    }
+}
